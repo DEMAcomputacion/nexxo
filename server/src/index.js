50760f52886f465259426a24801b05ed85a0e5f1
NEXXO Dev <dev@nexxo.com>
2026-03-03 23:38:03 -0300

    fix: use Express 5 wildcard syntax for SPA fallback route

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ app.use('/api/admin', adminRoutes);
 if (isProd) {
   const clientDist = path.join(__dirname, '../../client/dist');
   app.use(express.static(clientDist));
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(clientDist, 'index.html'));
   });
 }
