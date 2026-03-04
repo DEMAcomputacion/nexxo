a432932c4386a44410088e8a35efcf6530b96b01
NEXXO Dev <dev@nexxo.com>
2026-03-04 00:10:03 -0300

    fix: update PrismaPg to Prisma 7 API (connectionString config, not Pool)

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,16 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
 
-async function createPrismaClient() {
-  const { Pool } = pg;
-  const pool = new Pool({ 
-    connectionString: process.env.DATABASE_URL 
-  });
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter });
-}
-
-const prisma = await createPrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
